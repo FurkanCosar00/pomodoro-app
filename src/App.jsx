282b8e01e88f2303231d4dce715798918b2d6be0
Furkan <furkancosar2005@gmail.com>
2024-09-20 21:17:49 +0300

    sayaç çalışıyor

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,53 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
   const [timer, setTimer] = useState("1:00");
-  return (
-    <div className="container">
-      
-    </div>
+  const [isStarted, setIsStarted] = useState(false);
+
+  useEffect(() => {
+    let interval;
+    let minutes = +timer.split(":")[0];
+    let seconds = +timer.split(":")[1];
+
+    if(isStarted) {
+      interval = setInterval(() => {
+        if(seconds === 0) {
+          if(minutes == 0 && seconds == 0) {
+            console.log("bitti");
+            clearInterval(interval);
+            setIsStarted(false);
+            return;
+          }
+          seconds = 59;
+          minutes--;
+        }
+
+        seconds--;
+        setTimer(`${minutes}:${seconds}`);
+        console.log(minutes, seconds);
+      }, 1000)
+    }
+
+    return () => {
+      clearInterval(interval);
+    }
+  }, [isStarted]);
+
+  function handlePomodoro(e) {
+    setTimer(e.target.value);
+  }
+
+  return ( 
+    <>
+      {timer}
+      <br />
+      <button onClick={() => setIsStarted(!isStarted)}>
+        {isStarted ? "durdur" : "başlat"}
+      </button>
+
+      <input onChange={(e) => handlePomodoro(e)} value={timer} type="text" />
+    </>
   )
 }
 
